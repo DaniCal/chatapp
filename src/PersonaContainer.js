1837b8822afdb51c294672a6012da76cb61ea135
Daniel Lohse <daniel.lenerd.lohse@gmail.com>
2017-07-31 16:01:05 +0200

    Persona layout added to project

--- src/PersonaContainer.js
+++ src/PersonaContainer.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import './PersonaContainer.css'
+import persona from './portrait.png';
+
 
 class PersonaContainer extends React.Component {
   render(){
     return(
       <div className="PersonaContainer">
-        <h2>Hallo</h2>
+        <div>
+          <div className="PersonaElementContainer">
+            <p className="PersonaName">Edgar</p>
+            <p className="PersonaAge">29 years</p>
+          </div>
+          <div className="PersonaImageContainer">
+            <img src={persona} className="PersonaImage"/>
+          </div>
+        </div>
+        <div>
+          <p className="Title JobTitle">Job</p>
+          <p className="Attribute PersonaJob">HR-Manager</p>
+          <p className="Title NationalityTitle">Nationality</p>
+          <p className="Attribute PersonaNationality">German</p>
+        </div>
+        <a className="ExportButton" href="#export" >EXPORT</a>
+        
       </div>
   )};
 }
